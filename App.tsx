c562ffee7a524d3b81e7140c66ec4c5038858575
ankit-tailor <tailorankit3345@gmail.com>
2024-01-14 22:25:39 +0530

    feat: card swipe animation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,237 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import Animated, {
+  FadeInDown,
+  FadeInUp,
+  interpolate,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
+
+const width = Dimensions.get("window").width;
+const LAST_SWIPABLE_POINT = 120;
+const DURATION = 500;
+
+const Card = ({ style, children, updateArr, index, swipe, setSwipe }: any) => {
+  const offset = useSharedValue(0);
+  const absoluteX = useSharedValue(0);
+
+  const pan = Gesture.Pan()
+    .onChange((event) => {
+      if (index !== 0) return;
+
+      const absX = event.absoluteX;
+      const translationX = event.translationX;
+
+      offset.value = translationX;
+      absoluteX.value = absX;
+    })
+    .onFinalize((event) => {
+      if (index !== 0) return;
+      const absX = event.absoluteX;
+
+      absoluteX.value = absX;
+
+      if (
+        absoluteX.value <= LAST_SWIPABLE_POINT ||
+        Math.abs(width - absoluteX.value) <= LAST_SWIPABLE_POINT
+      ) {
+        if (absoluteX.value <= LAST_SWIPABLE_POINT) {
+          offset.value = withTiming(-width, {}, () => {
+            offset.value = withTiming(0);
+            runOnJS(updateArr)();
+          });
+        } else if (Math.abs(width - absoluteX.value) <= LAST_SWIPABLE_POINT) {
+          offset.value = withTiming(width, {}, () => {
+            offset.value = withTiming(0);
+            runOnJS(updateArr)();
+          });
+        }
+      } else {
+        offset.value = withTiming(0);
+      }
+    });
+
+  React.useEffect(() => {
+    if (swipe === "LEFT") {
+      offset.value = withSequence(
+        withTiming(-width / 3, { duration: DURATION }),
+        withDelay(
+          100,
+          withTiming(-width, { duration: DURATION }, () => {
+            offset.value = withTiming(0);
+            runOnJS(setSwipe)("");
+            runOnJS(updateArr)();
+          })
+        )
+      );
+    } else if (swipe === "RIGHT") {
+      offset.value = withSequence(
+        withTiming(width / 5, { duration: DURATION }),
+        withDelay(
+          100,
+          withTiming(width, { duration: DURATION }, () => {
+            offset.value = withTiming(0);
+            runOnJS(setSwipe)("");
+            runOnJS(updateArr)();
+          })
+        )
+      );
+    }
+  }, [swipe]);
+
+  const styles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: offset.value,
+        },
+        {
+          scale: 1 - index * 0.15,
+        },
+        {
+          rotate:
+            index === 0
+              ? `${interpolate(
+                  offset.value,
+                  [0, LAST_SWIPABLE_POINT],
+                  [0, 8]
+                )}deg`
+              : "0deg",
+        },
+        {
+          translateY:
+            index === 0
+              ? interpolate(offset.value, [0, LAST_SWIPABLE_POINT], [0, -20])
+              : 0,
+        },
+      ],
+    };
+  });
+
+  return (
+    <GestureDetector gesture={pan}>
+      <Animated.View
+        // entering={FadeInUp}
+        // exiting={FadeInDown}
+        style={[
+          {
+            height: 300,
+            width: 300,
+            borderRadius: 20,
+            position: "absolute",
+            transformOrigin: "bottom",
+            opacity: 1 - index * 0.5,
+          },
+          style,
+          styles,
+        ]}
+      >
+        {children}
+      </Animated.View>
+    </GestureDetector>
+  );
+};
 
 export default function App() {
+  const [arr, setArr] = useState(["red", "blue", "pink", "yellow", "teal"]);
+  const [swipe, setSwipe] = useState("");
+
+  const updateArr = () => {
+    const firsrElement = arr[0];
+    const newArr = arr.slice(1);
+    newArr.push(firsrElement);
+    setArr(newArr);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+        <View
+          style={{
+            position: "relative",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "red",
+            flex: 1,
+          }}
+        >
+          {arr.map((ele, index) => (
+            <Card
+              key={ele}
+              updateArr={updateArr}
+              index={index}
+              swipe={index === 0 ? swipe : ""}
+              setSwipe={setSwipe}
+              style={{
+                zIndex: arr.length - index,
+                bottom: index === 0 ? index * 30 : index * 50,
+                backgroundColor: "#fff",
+                display: index > 1 ? "none" : "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: "bold",
+                  color: "#5C98AA",
+                }}
+              >
+                Drink 5 glass of water a day.
+              </Text>
+            </Card>
+          ))}
+        </View>
+      </View>
+      <View
+        style={{
+          justifyContent: "space-between",
+          flexDirection: "row",
+          width: "100%",
+          paddingHorizontal: 32,
+          marginTop: 32,
+          flex: 1,
+        }}
+      >
+        <Pressable
+          style={{
+            paddingHorizontal: 16,
+            backgroundColor: "#f1f1f1",
+          }}
+          onPress={() => setSwipe("LEFT")}
+        >
+          <Text>Previous</Text>
+        </Pressable>
+        <Pressable
+          style={{
+            paddingHorizontal: 16,
+            backgroundColor: "#f1f1f1",
+          }}
+          onPress={() => setSwipe("RIGHT")}
+        >
+          <Text>Next</Text>
+        </Pressable>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#98B9D4",
   },
 });
---
+++ Gyroscope.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Gyroscope } from "expo-sensors";
+
+export default function App() {
+  const [{ x, y, z }, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState<any>(null);
+
+  const _slow = () => Gyroscope.setUpdateInterval(1000);
+  const _fast = () => Gyroscope.setUpdateInterval(16);
+
+  const _subscribe = () => {
+    setSubscription(
+      Gyroscope.addListener((gyroscopeData: any) => {
+        setData(gyroscopeData);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {/* <Text style={styles.text}>Gyroscope:</Text>
+      <Text style={styles.text}>x: {x}</Text>
+      <Text style={styles.text}>y: {y}</Text>
+      <Text style={styles.text}>z: {z}</Text> */}
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={subscription ? _unsubscribe : _subscribe}
+          style={styles.button}
+        >
+          <Text>{subscription ? "On" : "Off"}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={_slow}
+          style={[styles.button, styles.middleButton]}
+        >
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          backgroundColor: "red",
+          height: 100,
+          width: 100,
+          alignSelf: "center",
+          borderRadius: 999,
+          marginTop: 40,
+          transform: [
+            {
+              translateX: x * 10,
+            },
+            {
+              translateY: y * 10,
+            },
+          ],
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10,
+  },
+  text: {
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#ccc",
+  },
+});
